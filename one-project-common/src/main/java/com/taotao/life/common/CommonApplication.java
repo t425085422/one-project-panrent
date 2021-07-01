e1da05d07ae18905d56286066ccc27444b7d0faa
taoyilun <tyl@stdsxy.com>
2021-07-01 15:40:52 +0800

    test

--- one-project-common/src/main/java/com/taotao/life/common/CommonApplication.java
+++ one-project-common/src/main/java/com/taotao/life/common/CommonApplication.java
@@ -2,9 +2,20 @@ package com.taotao.life.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class CommonApplication {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "hello world！";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
