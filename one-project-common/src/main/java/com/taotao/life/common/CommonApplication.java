a6775b069cbca696ec4c72cf31558e1e5ba7d2f8
taoyilun <tyl@stdsxy.com>
2021-07-02 17:11:53 +0800

    提交网关以及注册中心

--- one-project-common/src/main/java/com/taotao/life/common/CommonApplication.java
+++ one-project-common/src/main/java/com/taotao/life/common/CommonApplication.java
@@ -7,15 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class CommonApplication {
-
-    @RequestMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "hello world！";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
